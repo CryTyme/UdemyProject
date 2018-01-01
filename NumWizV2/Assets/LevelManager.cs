810234cc22c1ff14935d4bce645587385bbe152d
CryTyme <34171378+CryTyme@users.noreply.github.com>
2018-01-01 14:05:56 -0500

    NumbWizV2
    Ver2 NumberWizard(UI)

---
+++ NumWizV2/Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name) {
+        Debug.Log("Level load request for: " + name);
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+
+    public void QuitRequest() {
+        Debug.Log("Quit request pressed");
+        Application.Quit();
+    }
+}
